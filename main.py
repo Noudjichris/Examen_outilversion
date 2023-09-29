1cff658f42cf25118f3a8cff1c47b22af0126050
Noudjichris <noudjichrist87@gmail.com>
2023-09-29 20:33:23 +0100

    Creation methode Création des méthodes pour créer des fichiers et dossiers du projet

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # MBAIGOLNOUDJI CHRISTIAN MASTER II Data Analyst
 
+from pathlib import Path
+import shutil
+import pandas as pd
+
+# MBAIGOLNOUDJI CHRISTIAN MASTER II Data Analyst
+def create_repertories(dir_list):
+    list(map(lambda x: Path(x).mkdir(parents=True, exist_ok=True), dir_list))
+
+def create_files(file_list):
+    list(map(lambda x: Path(x).touch(exist_ok=True), file_list))
