51f38fd161d9a6c646fc190833b1607e6ed12978
Noudjichris <noudjichrist87@gmail.com>
2023-09-29 20:35:25 +0100

    Création des listes Création des listes pour listes des fichiers et dossiers

--- main.py
+++ main.py
@@ -10,3 +10,8 @@ def create_repertories(dir_list):
 
 def create_files(file_list):
     list(map(lambda x: Path(x).touch(exist_ok=True), file_list))
+
+def dir_list():
+    return ["data/cleaned","data/processed","data/raw", "docs", "models", "notebooks", "reports", "src"]
+def file_list():
+    return ["LICENSE", "Makefile", "README.md","notebooks/main.ipynb", ".gitignore", "requirements.txt","src/utils.py","src/process.py","src/train.py"]
